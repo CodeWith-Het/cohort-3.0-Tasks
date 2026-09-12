6c08bcb069a58706554e705f35affc98e4f5cb74
CodeWith-Het <hetp1065@gmail.com>
2026-09-12 09:45:25 -0500

    __

--- Practice/Day1/backend/src/models/note.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-const noteSchemaModel = new mongoose.Schema({
-    title: String,
-    description:String
-})
-
-const noteModel = mongoose.model("note", noteSchemaModel)
-module.exports = noteModel
